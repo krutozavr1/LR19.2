6ca58192cf4dd71ff0e8e74729d689cfdc95db2b
krutozavr1 <ivannsharipkov@gmail.com>
2023-03-24 12:34:11 +0300

    fixed idz

--- python/idz/idz1.py
+++ python/idz/idz1.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import json
-import jsonschema
+import sys
+import click
 
 """
 Написать программу, которая считывает текст из файла и выводит на экран сначала
@@ -10,35 +11,27 @@ import jsonschema
 """
 
 def compress_to_json(content):
-    with open(save_file_name, "w", encoding="utf-8") as fout:
+    with open("data.json", "w", encoding="utf-8") as fout:
         json.dump(content, fout, ensure_ascii=False, indent=4)
 
-def valid_check(file_to_check):
-    with open(save_file_name, "r", encoding="utf-8") as fin:
-        schema = json.load(fin)
-    with open(file_to_check, "r", encoding="utf-8") as fin:
-        jsonschema.validate(json.load(fin), schema = schema)
+def print_sentences_in_cmd(sentences):
+    for s in sentences:
+        click.echo(s)
 
 if __name__ == "__main__":
-    save_file_name = input()
-    file_to_check = input()
-
-    divided_sentences = {}
+    divided_sentences = sys.argv[1:]
     exclamation_sentences = []
     question_sentences = []
 
-    with open("test_for_Idz1.txt", "r", encoding="utf-8") as f:
-        sentences = f.readlines()
+    for i in divided_sentences:
+        if i.find("!") != -1:
+            exclamation_sentences.append(i)
+        if i.find("?") != -1:
+            question_sentences.append(i)
 
-        for i in sentences:
-            if i.find("!") != -1:
-                exclamation_sentences.append(i)
-            if i.find("?") != -1:
-                question_sentences.append(i)
 
-    divided_sentences["exclamation_sentences"] = exclamation_sentences
-    divided_sentences["question_sentences"] = question_sentences
+    compress_to_json(exclamation_sentences)
+    compress_to_json(question_sentences)
 
-    compress_to_json(divided_sentences)
-    valid_check(file_to_check)
-
+    print_sentences_in_cmd(exclamation_sentences)
+    print_sentences_in_cmd(question_sentences)
